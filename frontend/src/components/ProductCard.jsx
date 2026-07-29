3d642a7ab463ebbe56c07ef17b43a7cb4a8a364d
Matheus Simões <matheussimoes3356@gmail.com>
2026-07-29 19:07:08 -0300

    alterando padding

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -69,7 +69,7 @@ export const ProductCard = React.memo(({ product, onClick, isBestseller = false 
               fontSize: '0.78rem',
               letterSpacing: '0.1em',
               textTransform: 'uppercase',
-              gap: '5px',
+              gap: '15px',
             }}
           >
             Sem imagem
