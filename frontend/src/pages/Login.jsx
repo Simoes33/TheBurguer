1ae3ff132455453a4b3dae54ef14b79c9f65b89f
Matheus Simões <matheussimoes3356@gmail.com>
2026-07-29 22:02:06 -0300

    alterando hero register desktop

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -49,7 +49,7 @@ export const Login = () => {
       <div className="login-image-panel" style={{
         backgroundImage: [
           'linear-gradient(to right, rgba(8,8,8,0.1) 0%, rgba(8,8,8,0.75) 100%)',
-          "url('https://unsplash.com/pt-br/fotografias/uma-pessoa-esta-grelhando-um-hamburguer-em-uma-grelha-QXxDAuqYcFg')",
+          "url('https://images.unsplash.com/photo-1676887451694-713019c9031f?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         ].join(', '),
       }}>
         <div>
